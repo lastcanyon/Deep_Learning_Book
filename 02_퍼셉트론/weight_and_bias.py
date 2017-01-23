3783d61f115201fdc8a0067df42db0f273bf43f3
Sanghee <lastcanyon@gmail.com>
2017-01-23 16:30:59 +0900

    주석 추가

--- 02_퍼셉트론/weight_and_bias.py
+++ 02_퍼셉트론/weight_and_bias.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+#AND게이트
 def AND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([0.5, 0.5])
@@ -11,7 +12,7 @@ def AND(x1, x2):
         return 1
 print("AND게이트", AND(0, 0), AND(1, 0), AND(0, 1), AND(1, 1))
 
-
+#NAND게이트
 def NAND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([-0.5, -0.5])
@@ -24,8 +25,7 @@ def NAND(x1, x2):
         return 1
 print("NAND게이트", NAND(0, 0), NAND(1, 0), NAND(0, 1), NAND(1, 1))
 
-
-
+#OR게이트
 def OR(x1, x2):
     x = np.array([x1, x2])
     w = np.array([0.5, 0.5])
@@ -37,7 +37,7 @@ def OR(x1, x2):
         return 1
 print("OR게이트", OR(0, 0), OR(1, 0), OR(0, 1), OR(1, 1))
 
-
+#XOR게이트
 def XOR(x1, x2):
     s1 = NAND(x1, x2)
     s2 = OR(x1, x2)
