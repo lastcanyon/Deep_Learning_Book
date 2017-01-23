5d464e8d0328989e0c747b5c1264ce9d50d5a83b
Sanghee <lastcanyon@gmail.com>
2017-01-23 15:43:05 +0900

    XOR 게이트 추가 및 함수 return값 설정 방식으로 수정

--- 02_퍼셉트론/weight_and_bias.py
+++ 02_퍼셉트론/weight_and_bias.py
@@ -6,14 +6,11 @@ def AND(x1, x2):
     b = -0.7
     tmp = np.sum(x*w) + b
     if tmp <= 0:
-        print(0)
+        return 0
     else:
-        print(1)
-print("AND게이트")
-AND(0, 0)
-AND(1, 0)
-AND(0, 1)
-AND(1, 1)
+        return 1
+print("AND게이트", AND(0, 0), AND(1, 0), AND(0, 1), AND(1, 1))
+
 
 def NAND(x1, x2):
     x = np.array([x1, x2])
@@ -22,14 +19,11 @@ def NAND(x1, x2):
     tmp = np.sum(w*x) + b
 
     if(tmp <= 0):
-        print(0)
+        return 0
     else:
-        print(1)
-print("NAND게이트")
-NAND(0, 0)
-NAND(1, 0)
-NAND(0, 1)
-NAND(1, 1)
+        return 1
+print("NAND게이트", NAND(0, 0), NAND(1, 0), NAND(0, 1), NAND(1, 1))
+
 
 
 def OR(x1, x2):
@@ -38,11 +32,15 @@ def OR(x1, x2):
     b = -0.2
     tmp = np.sum(w*x) + b
     if tmp <= 0:
-        print(0)
+        return 0
     else:
-        print(1)
-print("OR게이트")
-OR(0, 0)
-OR(1, 0)
-OR(0, 1)
-OR(1, 1)
+        return 1
+print("OR게이트", OR(0, 0), OR(1, 0), OR(0, 1), OR(1, 1))
+
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+print("XOR게이트", XOR(0, 0), XOR(1, 0), XOR(0, 1), XOR(1, 1))
