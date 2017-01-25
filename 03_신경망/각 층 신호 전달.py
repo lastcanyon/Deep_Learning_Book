99e6a7c6eae56e827fff4a16b23d54d4acd0dee6
Sanghee <lastcanyon@gmail.com>
2017-01-25 14:33:30 +0900

    활성화 함수로 시그모이드 함수 사용

--- 03_신경망/각 층 신호 전달.py
+++ 03_신경망/각 층 신호 전달.py
@@ -4,6 +4,16 @@ X = np.array([1.0, 0.5])
 W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
 B1 = np.array([0.1, 0.2, 0.3])
 
-print(W1.shape)
-print(X.shape)
-print(B1.shape)
+A1 = np.dot(X, W1) +B1
+
+print('W1.shape', W1.shape)
+print('X.shape', X.shape)
+print('B1.shape', B1.shape)
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x)) #넘파이 배열을 반환
+
+Z1 = sigmoid(A1)
+
+print('A1', A1)
+print('Z1', Z1)
