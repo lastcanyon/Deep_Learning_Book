b017fd8839320df16951af6796992ea6aa8c9546
Sanghee <lastcanyon@gmail.com>
2017-01-28 18:09:37 +0900

    다른 숫자 레이블로 값이 주어졌을 때의 교차 엔트로피 오차

--- 04_신경망학습/배치용CEE.py
+++ 04_신경망학습/배치용CEE.py
@@ -6,4 +6,5 @@ def cross_entropy_error(y, t):
         y = y.reshape(1, y.size)
 
     batch_size = y.shape[0]
-    return -np.sum(t * np.log(y)) / batch_size
+    return -np.sum(t * np.log(y[np.arrange(batch_size), t])) / batch_size
+    # 원-핫 인코딩이 아니라 2나 7 등의 숫자 레이블이 주어졌을 때 교차 엔트로피 오차
